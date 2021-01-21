1bbf103d212e896ffdc2cb3a9261af4d5a179a27
Ben Carlson <bjcarlson42@outlook.com>
2021-01-21 10:50:39 -0500

    add chakra-ui to next.js

---
+++ pages/_app.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { ChakraProvider, ColorModeProvider, useColorMode } from '@chakra-ui/react'
+import customTheme from '../styles/theme'
+import { Global, css } from '@emotion/react'
+
+const GlobalStyle = ({ children }) => {
+  const { colorMode } = useColorMode()
+
+  return (
+    <>
+      <Global
+        styles={css`
+          ::selection {
+            background-color: #90CDF4;
+            color: #fefefe;
+          }
+          ::-moz-selection {
+            background: #ffb7b7;
+            color: #fefefe;
+          }
+          html {
+            min-width: 356px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+            background: ${colorMode === 'light' ? 'white' : '#171717'};
+          }
+        `}
+      />
+      {children}
+    </>
+  )
+}
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider resetCSS theme={customTheme}>
+      <ColorModeProvider
+        options={{
+          initialColorMode: "light",
+          useSystemColorMode: true,
+        }}
+      >
+        <GlobalStyle>
+          <Component {...pageProps} />
+        </GlobalStyle>
+      </ColorModeProvider>
+    </ChakraProvider>
+  )
+}
+
+export default MyApp
---
+++ pages/index.js
@@ -0,0 +1,44 @@
+import Head from 'next/head'
+import {
+  useColorMode,
+  Heading,
+  Text,
+  Flex,
+  Stack
+} from '@chakra-ui/react'
+
+import Container from '../components/Container'
+
+export default function Index() {
+  const { colorMode } = useColorMode()
+  const colorSecondary = {
+    light: 'gray.700',
+    dark: 'gray.400'
+  }
+  return (
+    <Container>
+      <Head>
+        <title>Home - Benjamin Carlson</title>
+      </Head>
+      <Stack
+        as="main"
+        spacing={8}
+        justifyContent="center"
+        alignItems="flex-start"
+        m="0 auto 4rem auto"
+        maxWidth="700px"
+        px={2}
+      >
+        <Flex
+          flexDirection="column"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          maxWidth="700px"
+        >
+          <Heading mb={2}>Hi, I'm Benjamin Carlson</Heading>
+          <Text color={colorSecondary[colorMode]}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ante nunc, finibus sit amet purus quis, congue vulputate ipsum. Phasellus lobortis bibendum orci, quis imperdiet lectus imperdiet porttitor.</Text>
+        </Flex>
+      </Stack>
+    </Container>
+  )
+}
